d26e3c7f3d25469a55cb683b64ad210cc69c5ebc
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-05-24 01:06:25 +0500

    feat: add schema for the prepopulatin the postgres container

--- server/src/postgresClient/schema.sql
+++ server/src/postgresClient/schema.sql
@@ -1,42 +1,44 @@
+CREATE DATABASE lms WITH OWNER postgres;
+\c lms;
+
 CREATE TABLE users (
     id UUID PRIMARY KEY,
-    firstName VARCHAR(128),
-    secondName VARCHAR(128),
-    email VARCHAR(200),
-    password VARCHAR(300),
-    role VARCHAR(50),
-    isSubscribed BOOLEAN,
-    freeTokens SMALLINT
+    first_name VARCHAR(128) NOT NULL,
+    last_name VARCHAR(128) NOT NULL,
+    email VARCHAR(200) UNIQUE NOT NULL,
+    password VARCHAR(300) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    is_subscribed BOOLEAN NOT NULL,
+    free_tokens SMALLINT NOT NULL,
+    login_method VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE test_data (
     id Serial PRIMARY KEY,
-    subject VARCHAR(128),
-    statment TEXT,
-    optionA TEXT,
-    optionB TEXT,
-    optionC TEXT,
-    correct VARCHAR(10),
+    subject VARCHAR(128) NOT NULL,
+    statment TEXT NOT NULL,
+    option_a TEXT NOT NULL,
+    option_b TEXT NOT NULL,
+    option_c TEXT NOT NULL,
+    correct VARCHAR(10) NOT NULL,
     explaination TEXT,
-    paper_year SMALLINT,
-    difficulty VARCHAR(10)
+    paper_year SMALLINT NOT NULL,
+    difficulty VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE user_overall_data (
     userId UUID REFERENCES users,
-    subject VARCHAR(128),
-    totalSolved INT,
-    totalCorrect INT,
-    totalIncorrect INT
+    subject VARCHAR(128) NOT NULL,
+    total_solved INT NOT NULL,
+    total_correct INT NOT NULL,
+    total_incorrect INT NOT NULL
 );
 
-CREATE DATABASE lms WITH OWNER postgres;
-\c lms;
-CREATE TABLE random (
-  name VARCHAR(128),
-  email VARCHAR(128)
+CREATE TABLE tokens ( 
+    id UUID REFERENCES users,
+    token VARCHAR(300) NOT NULL
 );
-
-INSERT INTO random(name, email) VALUES ('emily', 'emily@gmail.com');
-
  
+INSERT INTO users (id, first_name, last_name, email, password, role, is_subscribed  , free_tokens, login_method) 
+VALUES ( 'f415284b-87b4-4206-a79f-5bd61b00de97', 'hamza', 'saleem', 'hamza@gmail.com', 
+'$2a$10$Aqb2UA6V6n/p6tJZPUWvre3HnSef3WX/v822L7x9RxHT0mJXkx9w6', 'user', false, 300, 'normal');
