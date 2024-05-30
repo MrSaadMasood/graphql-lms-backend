960d6bc73b0690e5a5e754b802fdd8b309d318cd
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-05-30 22:39:48 +0500

    feat: add string and date validators

---
+++ server/src/utils/helperFunctions.ts
@@ -0,0 +1,10 @@
+import { z } from "zod"
+
+export function dateValidator(value: unknown) {
+  return z.date({ invalid_type_error: "expected date string" }).parse(value)
+}
+
+export function stringValidator(value: unknown) {
+  if (typeof value === "string" && value.length > 0) return value
+  throw new TypeError("Empty string is recieved")
+}
