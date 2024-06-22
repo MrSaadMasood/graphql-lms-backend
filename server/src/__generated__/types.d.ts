de36f20f5919f8d2262e245aa890e4b7ddf30f59
MrSaadMasood <mrsaadmasood1@gmail.com>
2024-06-22 18:49:05 +0500

    feat: add missing types

--- server/src/__generated__/types.d.ts
+++ server/src/__generated__/types.d.ts
@@ -1,2 +1,16 @@
 export type UserRole = 'user' | 'admin';
 export type UserLoginMethod = 'normal' | 'google';
+export type Subsription_type = "none" | "temp" | "permanent"
+export type UserInfoToCreateToken = {
+  id: string,
+  first_name: string,
+  last_name: string,
+  email: string,
+  password: string,
+  role: UserRole,
+  subscription_type: Subsription_type,
+  free_tokens: number,
+  login_method: UserLoginMethod
+};
+
+export type UserContext = UserInfoToCreateToken | null
